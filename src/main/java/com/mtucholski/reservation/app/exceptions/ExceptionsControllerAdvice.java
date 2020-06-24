91e33465f210d31114196091e20a7601a79c3e95
mtucholski <mateusz.tucholski@atos.net>
2020-06-24 08:18:09 +0200

    add exception handler

---
+++ src/main/java/com/mtucholski/reservation/app/exceptions/ExceptionsControllerAdvice.java
@@ -0,0 +1,37 @@
+package com.mtucholski.reservation.app.exceptions;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Component
+public class ExceptionsControllerAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        ObjectMapper mapper = new ObjectMapper();
+        ErrorInfo errorInfo = new ErrorInfo(e);
+        String respJSONstring = "{}";
+        try {
+            respJSONstring = mapper.writeValueAsString(errorInfo);
+        } catch (JsonProcessingException e1) {
+            e1.printStackTrace();
+        }
+        return ResponseEntity.badRequest().body(respJSONstring);
+    }
+
+    private static class ErrorInfo {
+        public final String className;
+        public final String exMessage;
+
+        public ErrorInfo(Exception ex) {
+            this.className = ex.getClass().getName();
+            this.exMessage = ex.getLocalizedMessage();
+        }
+    }
+
+}
